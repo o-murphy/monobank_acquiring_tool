efa05a7d4953e0d8f618c6eca9e643c6dce5e9ed
Murphy <thehelixpg@gmail.com>
2021-12-24 17:21:41 +0200

    update index.html

--- init.js
+++ init.js
@@ -71,18 +71,13 @@
             // Default options are marked with *
             const response = await fetch(url, {
                 method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
                 headers: {
                     "Content-Type": "application/json",
                     "X-Token": aToken
                 },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *client
                 body: JSON.stringify(data) // body data type must match "Content-Type" header
             });
-            return await response; // parses JSON response into native JavaScript objects
+            return await response.json(); // parses JSON response into native JavaScript objects
         }
 
         function sendTgAdminMsg() {
