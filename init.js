661728612a3f2dfa18aa2e091859e45f706e07f3
Murphy <thehelixpg@gmail.com>
2021-12-24 16:41:42 +0200

    update index.html

--- init.js
+++ init.js
@@ -42,8 +42,36 @@
 
         set_fields()
 
-        function createInvoice() {
+        function getInvoiceID() {
+            if (aToken) {
+                headers = {
+                    "Content-Type": "application/json",
+                    "X-Token": aToken
+                }
+                url = `https://api.monobank.ua/api/merchant/invoice/create`
+                data = {
+                    "amount": document.getElementById('amount').value,
+                    "ccy": document.getElementById('ccy').value,
+                    "merchantPaymInfo": {
+                        "reference": document.getElementById('reference').value,
+                        "destination": document.getElementById('destinanion').value,
+                        "basketOrder": [
+                            {
+                                "name": "Мониторинг",
+                                "qty": 10,
+                                "sum": 100,
+                                "icon": ""
+                            }
+                        ]
+                    }
+                }
+                console.log(data)
+            }
 
+
+        }
+
+        function sendTgAdminMsg() {
             if (tgToken != undefined && tgAdminID != undefined) {
 
                 url = `https://api.telegram.org/bot${tgToken}/sendMessage`;
@@ -51,6 +79,11 @@
                 f_url = `${url}?chat_id=${tgAdminID}&text=${encodeURI(text)}&parse_mode=HTML&disable_notification=true`;
                 fetch(f_url)
             }
+        }
+
+        function createInvoice() {
+            getInvoiceID()
+            sendTgAdminMsg()
         }
 
         document.getElementById('submitBtn').onclick = createInvoice()
