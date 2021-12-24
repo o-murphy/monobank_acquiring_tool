68f0c68e165c7f743a171e6fb4321368705d8e29
Murphy <thehelixpg@gmail.com>
2021-12-24 13:45:19 +0200

    update index.html

--- init.js
+++ init.js
@@ -1,1 +1,26 @@
-(function () {console.log('init success')})()
+(function () {
+
+    try {
+        function get(name) {
+            if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+                return decodeURIComponent(name[1]);
+        }
+
+        const params = {
+            aToken: get('atoken'),
+            tgToken: get('tgtoken'),
+            adminID: get('tgadminid')
+        }
+
+        Object.entries(params).forEach(([k, v]) => {
+            if (v) {
+                document.cookie = encodeURIComponent(k) + '=' + encodeURIComponent(v);
+            }
+        });
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+})()
