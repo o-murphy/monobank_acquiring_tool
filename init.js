cb4342e4eb550a9f2e8a7d4649a88be8ce4c0c67
Murphy <thehelixpg@gmail.com>
2021-12-24 15:50:34 +0200

    update index.html

--- init.js
+++ init.js
@@ -6,6 +6,13 @@
                 return decodeURIComponent(name[1]);
         }
 
+        function getCookie(name) {
+            let matches = document.cookie.match(new RegExp(
+              "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+            ));
+            return matches ? decodeURIComponent(matches[1]) : undefined;
+          }
+
         const params = {
             aToken: get('atoken'),
             tgToken: get('tgtoken'),
@@ -17,9 +24,15 @@
                 document.cookie = encodeURIComponent(k) + '=' + encodeURIComponent(v);
             }
         });
-        let date = new Date(Date.now() + (10*86400e3));
+        let date = new Date(Date.now() + (31*86400e3));
         date = date.toUTCString();
         document.cookie = "expires=" + date
+
+        function set_fields() {
+            document.getElementById('aquireToken')
+        }
+
+
 
 
     } catch (error) {
