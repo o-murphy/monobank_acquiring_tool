c1c9ede966369aa699d0335155e730a9d8c8505f
Murphy <thehelixpg@gmail.com>
2021-12-24 17:09:48 +0200

    update index.html

--- init.js
+++ init.js
@@ -44,10 +44,6 @@
 
         function getInvoiceID() {
             if (aToken) {
-                headers = {
-                    "Content-Type": "application/json",
-                    "X-Token": aToken
-                }
                 url = `https://api.monobank.ua/api/merchant/invoice/create`
                 data = {
                     "amount": parseInt(document.getElementById('amount').value),
@@ -65,10 +61,28 @@
                         ]
                     }
                 }
-                console.log(data)
+                postData(url, data).then((data) => {console.log(data)})
             }
 
 
+        }
+
+        async function postData(url = '', data = {}) {
+            // Default options are marked with *
+            const response = await fetch(url, {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers = {
+                    "Content-Type": "application/json",
+                    "X-Token": aToken
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer', // no-referrer, *client
+                body: JSON.stringify(data) // body data type must match "Content-Type" header
+            });
+            return await response.json(); // parses JSON response into native JavaScript objects
         }
 
         function sendTgAdminMsg() {
