c2940595173de392126b1f6ca720e2139d9274c3
Murphy <thehelixpg@gmail.com>
2022-01-12 16:22:40 +0200

    update

--- wln.js
+++ wln.js
@@ -1,44 +1,47 @@
-function wlnLoginHash () {
+function wlnLoginHash() {
     sess = wialon.core.Session.getInstance()
     sess.initSession(`https://${getCookie('baseUrl')}`)
     sess.loginAuthHash(getCookie('wlnHash'), "", // trying login 
-            function (code, data) { // login callback
-                if (code, data) console.log(wialon.core.Errors.getErrorText(code)); // login failed, print error
-                else {
-                    console.log("Logged successfully via hash");
-                } // login succeed
-            })
+        function (code, data) { // login callback
+            if (code, data) console.log(wialon.core.Errors.getErrorText(code)); // login failed, print error
+            else {
+                console.log("Logged successfully via hash");
+            } // login succeed
+        })
 }
 
 function wlnLoginToken() {
     sess = wialon.core.Session.getInstance()
     sess.initSession(`https://${getCookie('baseUrl')}`)
     sess.loginToken(getCookie('wlnToken'), "", // trying login 
-            function (code, data) { // login callback
-                if (code, data) console.log(wialon.core.Errors.getErrorText(code)); // login failed, print error
-                else {
-                    console.log("Logged successfully via token");
-                } // login succeed
-            })
+        function (code, data) { // login callback
+            if (code, data) console.log(wialon.core.Errors.getErrorText(code)); // login failed, print error
+            else {
+                console.log("Logged successfully via token");
+            } // login succeed
+        })
 }
 
 function getTokens() {
+    sess = wialon.core.Session.getInstance()
     sess.listTokens(
         sess.getCurrUser()._id, (code, data) => {
             if (code, data) console.log(wialon.core.Errors.getErrorText(code));
-            else if (data) {if (sess.getCurrUser().$$user_name && data.length > 0) {
-                            console.log(data)
-                       }
+            else if (data) {
+                if (sess.getCurrUser().$$user_name && data.length > 0) {
+                    console.log(data)
+                }
             }
         }
     )
 }
 
 function wlnUpdateToken() {
+    sess = wialon.core.Session.getInstance()
     sess.updateToken(
         'create',
         new Token, (code, data) => {
-            if (code) {console.log(wialon.core.Errors.getErrorText(code))}
+            if (code) { console.log(wialon.core.Errors.getErrorText(code)) }
             else {
                 document.cookie = "wlnToken=" + data.h
             }
@@ -55,7 +58,19 @@ class Token {
 }
 
 function getAccounts() {
-    
+    sess = wialon.core.Session.getInstance()
+    sess.loadLibrary("resourceAccounts")
+    let flags = wialon.item.Item.dataFlag.base | wialon.item.Item.dataFlag.billingProps;
+    sess.updateDataFlags(
+        [{ type: "type", data: "avl_resource", flags: flags, mode: 0 }],
+        function (code) {
+            if (code) { console.log(wialon.core.Errors.getErrorText(code)); return; }
+            let resources = sess.getItems("avl_resource");
+            accounts = resources.filter((r) => r.$$user_accountId === r._id).map(e => e._id)
+
+            sess.getAccountsData(accounts, 1, (code, data) => console.log(code, data))
+        }
+    )
 }
 
 if (getCookie('wlnHash') && getCookie('baseUrl') && !getCookie('wlnToken')) {
@@ -66,4 +81,3 @@ if (getCookie('wlnHash') && getCookie('baseUrl') && !getCookie('wlnToken')) {
 if (getCookie('baseUrl') && getCookie('wlnToken')) {
     wlnLoginToken()
 }
-    
